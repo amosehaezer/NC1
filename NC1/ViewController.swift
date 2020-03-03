6d1d42f0994f33d4de99e52d68730c0249aa20ed
Amos Ebenhaezer <amosehaezer@gmail.com>
2020-03-03 18:32:18 +0700

    Initial Commit

---
+++ NC1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NC1
+//
+//  Created by Amos Ebenhaezer on 03/03/20.
+//  Copyright © 2020 Amos Ebenhaezer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
